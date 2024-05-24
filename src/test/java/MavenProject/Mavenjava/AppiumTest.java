2b08c953ba333b8645a59dd19c21e2a02831e1ca
Roushan <rk.choudhary387@gmail.com>
2024-05-24 21:33:05 +0530

    Architect Asian Devlopement

--- src/test/java/MavenProject/Mavenjava/AppiumTest.java
+++ src/test/java/MavenProject/Mavenjava/AppiumTest.java
@@ -3,18 +3,19 @@ package MavenProject.Mavenjava;
 import org.testng.annotations.Test;
 
 public class AppiumTest {
-	
+
 	@Test
-	public void nativeAppAndrovid()
-	{
+	public void nativeAppAndrovid() {
 		System.out.println("nativeAppAndrovid");
-		
+
 	}
+
 	@Test
-	public void IOSApps()
-	{
-		System.out.println("IOSApps");
-		
+	public void IOSApps() {
+		System.out.println("IOSApps1");
+		System.out.println("IOSApps2");
+		System.out.println("IOSApps3");
+
 	}
 
 }
