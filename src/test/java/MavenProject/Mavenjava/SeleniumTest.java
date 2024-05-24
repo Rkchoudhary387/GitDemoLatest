b903bbe7e6d58b671fc03b6d6ebae07e931d5e04
Roushan <rk.choudhary387@gmail.com>
2024-05-24 18:09:25 +0530

    Indian guy

--- src/test/java/MavenProject/Mavenjava/SeleniumTest.java
+++ src/test/java/MavenProject/Mavenjava/SeleniumTest.java
@@ -3,20 +3,20 @@ package MavenProject.Mavenjava;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
-	//testing xml file from maven
-	//how to execute all test cases from test folder - mvn test
+	// testing xml file from maven
+	// how to execute all test cases from test folder - mvn test
 	@Test
-	
-	public void BowserAutomation()
-	{
-		System.out.println("BowserAutomation");
-		
+
+	public void BowserAutomation() {
+		System.out.println("Bowser Automation");
+		System.out.println("Web Automation");
+
 	}
+
 	@Test
-	public void elementsUI()
-	{
+	public void elementsUI() {
 		System.out.println("elementsUI");
-		
+
 	}
 
 }
