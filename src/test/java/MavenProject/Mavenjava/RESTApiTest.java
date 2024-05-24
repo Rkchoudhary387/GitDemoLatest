0da68cc48766c615c413322e866ee5b362f02ac0
Roushan <rk.choudhary387@gmail.com>
2024-05-24 15:46:59 +0530

    this is from asian time zone guy

--- src/test/java/MavenProject/Mavenjava/RESTApiTest.java
+++ src/test/java/MavenProject/Mavenjava/RESTApiTest.java
@@ -3,18 +3,19 @@ package MavenProject.Mavenjava;
 import org.testng.annotations.Test;
 
 public class RESTApiTest {
-	
+
 	@Test
-	public void postjira()
-	{
+	public void postjira() {
 		System.out.println("postjira");
-		
+		System.out.println("postjira2");
+		System.out.println("postjira3");
+
 	}
+
 	@Test
-	public void deleteTwitter()
-	{
+	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
-		
+
 	}
 
 }
